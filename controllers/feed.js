690b94ff24c9c1ee6e6c28c72fd0925a1539640d
DESTINYAS <destiny.ochonogor@gmail.com>
2022-10-18 21:09:39 +0100

    Error Handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -9,10 +9,13 @@ exports.getPost = (req, res, next) => {
 exports.creatPosts = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: "validation failed, entered data is incorrect",
-      errors: errors.array(),
-    });
+    const error = new Error("validation failed, entered data is incorrect");
+    error.statusCode = 422;
+    throw error;
+    // return res.status(422).json({
+    //   message: "validation failed, entered data is incorrect",
+    //   errors: errors.array(),
+    // });
   }
   const title = req.body.title;
   const content = req.body.content;
@@ -34,6 +37,9 @@ exports.creatPosts = (req, res, next) => {
       });
     })
     .catch((err) => {
-      console.log(err);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
     });
 };
